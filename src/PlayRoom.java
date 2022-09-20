1abf3b5e028ac99979acc2aef3ad82bce90b52f6
李尔 <1519301551@qq.com>
2022-09-20 15:52:33 +0800

    第三次修改

--- src/PlayRoom.java
+++ src/PlayRoom.java
@@ -3,18 +3,20 @@ import static java.lang.StrictMath.pow;
 public class PlayRoom {
     private static int maxTime=0;
     private  static int minTime=Integer.MAX_VALUE;
+    private static int antCount=5;
     public static void main(String[] args) {
-        int antCount = 5;
-        for (int i = 0; i < pow(2, antCount); i++) {
-            int currentTime;
-            CreepingGame cg = new CreepingGame(i);
-            currentTime = cg.playGame();
-            updateRecord(currentTime);
-            System.out.println("第" + (i+1) + "种情况time: "+cg.getCurrentTime());
-        }
+        startGame();
         //output!
         System.out.println("max time is: " + maxTime);
         System.out.println("min time is: " + minTime);
+    }
+    public static void startGame(){
+        for (int i = 0; i < pow(2, antCount); i++) {
+            int time;
+            CreepingGame cg = new CreepingGame(i);
+            time = cg.playGame();
+            updateRecord(time);
+        }
     }
     public static void updateRecord(int currentTime){
         if(currentTime>minTime && currentTime<=maxTime) return;
